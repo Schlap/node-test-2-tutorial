3a03211a0144764bfc525e6b70edbb54c7839ef2
Hadi <hadichalabi@gmail.com>
2014-11-17 17:16:07 +0000

    it works

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
   //* Username link click
   $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
   $('#btnAddUser').on('click', addUser);
+  $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 });
 
 //* Functions ================================================================
@@ -110,4 +111,43 @@ function addUser(event) {
         alert('Please fill in all fields');
         return false;
     }
+};
+
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
 };
